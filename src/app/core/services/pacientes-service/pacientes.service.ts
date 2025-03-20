d8c56647021a0206a3a003d51459fec90d1fac9b
GB1177 <gdd_11@hotmail.com>
2025-03-20 17:59:36 -0300

    Fix: Integração back end e front end, mudanças de config

--- src/app/core/services/pacientes-service/pacientes.service.ts
+++ src/app/core/services/pacientes-service/pacientes.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Paciente } from '../../models/pacientes/pacientes.model';
 import { environment } from '../../../environments/pacientes-environments/pacientes-environments';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -13,6 +15,11 @@ export class PacientesService {
   constructor(private http: HttpClient) {}
 
   getPacientes(): Observable<Paciente[]> {
-    return this.http.get<Paciente[]>(this.apiUrl);
+    return this.http.get<Paciente[]>(this.apiUrl).pipe(
+      catchError((error) => {
+        console.error('Erro ao buscar pacientes:', error);
+        return throwError(() => new Error('Erro ao carregar pacientes'));
+      })
+    );
   }
 }
